c7735201188df507055a46b29ab537144bfc61cb
Charles Yu <charles.yu@uwaterloo.ca>
2020-12-08 01:54:11 -0800

    Move Firebase initialization to App.js, remove from HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbSb1BnJBU9NyduA6BrG50PC5vlIK_koc",
+  authDomain: "mapstimator.firebaseapp.com",
+  projectId: "mapstimator",
+  storageBucket: "mapstimator.appspot.com",
+  messagingSenderId: "169137768653",
+  appId: "1:169137768653:web:54903a41fcdada51428184",
+  measurementId: "G-4LTHPF4V9T"
+};
 
 function App() {
+  firebase.initializeApp(firebaseConfig);
+
+  // REMOVE FOR PRODUCTION
+  // firebase.firestore().useEmulator("localhost", 8080);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hi there!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ItemList />
       </header>
     </div>
   );
+}
+
+class Item extends React.Component {
+  render() {
+    return(<li>{this.props.value}</li>);
+  };
+}
+
+class ItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    var list = [];
+    this.state = {
+      values: list
+    };
+  }
+
+  componentDidMount() {
+    var db = firebase.firestore();
+    var list = [];
+    db.collection('users').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+        console.log(doc.data());
+      });
+      console.log(list);
+      this.setState({
+        values: list
+      });
+    });
+  }
+
+  renderItem(value) {
+    return(<Item value={value} />);
+  }
+
+  render() {
+    var to_render = [];
+    this.state.values.forEach((value) => {to_render.push(this.renderItem(value))});
+    console.log(to_render);
+    return(<ul>
+            {to_render}
+           </ul>);
+  }
 }
 
 export default App;
